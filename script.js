7d0f5d8c4b612a27bd5e0ee50e87b7118220b79c
mojofill <79336116+mojofill@users.noreply.github.com>
2022-07-09 19:17:06 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -11,10 +11,17 @@ canvas.style.width = WIDTH;
 
 let simulationOn = false;
 
-const UNIT_WIDTH = 25;
-const RENDER_FPS = 60;
-const SIMULATION_FPS = 10;
+let UNIT_WIDTH = 25;
+const RENDER_FPS = 120;
+let SIMULATION_FPS = 100;
 let FPS = 60;
+
+const LIVE_CELL_COLOR = "rgb(0, 102, 102)";
+const DEAD_CELL_COLOR = "rgb(0, 51, 102)";
+
+document.body.style.backgroundColor = DEAD_CELL_COLOR;
+
+let randomBias = 0.5; // its always 50/50
 
 let gridMap = [];
 
@@ -28,12 +35,16 @@ const camera = {
     y: -gridMap[0].length / 4 * UNIT_WIDTH,
 }
 
+let addWidth = 0;
+
 const mouse = {
     x: 0,
     y: 0,
     left: false,
     right: false,
+    ctrl: false,
     shift: false,
+    t: false,
     cameraOffset: {
         lastHeldPosition: {
             x: null,
@@ -44,6 +55,52 @@ const mouse = {
     }
 }
 
+const keys = {
+    minus: false,
+    plus: false,
+}
+
+class Time {
+    currTime;
+    pastTime;
+    constructor() {
+        this.currTime = new Date().getTime() / 1000;
+        this.pastTime = new Date().getTime() / 1000;
+    }
+
+    get deltaTime() {
+        return this.currTime - this.pastTime;
+    }
+
+    updateTime() {
+        this.pastTime = this.currTime;
+        this.currTime = new Date().getTime() / 1000;
+    }
+}
+
+const time = new Time();
+
+class Templates {
+    GosperGliderGun() {
+        let map = [];
+        for (let y = 0; y < 11; y++) {
+            map.push([]);
+            for (let x = 0; x < 38; x++) {
+                map[y].push(false);
+            }
+        }
+
+        // right square
+        map[5][1] = map[5][2] = map[6][1] = map[6][2] = true;
+
+        // big gun part 1
+        map[3][13] = map[3][14] = map[4][12] = map[5][11] = map[6][11] = map[7][11] = map[8][12] = map[9][13] = map[9][14] = map[6][15] = map[4][16] = map[8][16] = map[5][17] = map[6][17] = map[7][17] = map[6][18] = true;
+
+        return map;
+    }
+}
+
+const templates = new Templates();
 
 let simulationFrameCount = 0;
 
@@ -54,10 +111,73 @@ function init() {
 
 function listeners() {
     document.addEventListener('keydown', (e) => {
-        mouse.shift = e.code === 'ShiftLeft' || e.code === 'ShiftRight';
+        mouse.ctrl = e.code === 'ControlLeft' || e.code === 'ControlRight';
+        mouse.shift = e.code === "ShiftLeft" || e.code === "ShiftRight";
+        keys.minus = e.code === "Minus";
+        keys.plus = e.code === "Equal";
+        mouse.t = e.code === "KeyT";
+
+        if (e.code === "ArrowDown") {
+            randomBias -= 0.01;
+            if (randomBias <= 0) randomBias = 0;
+            console.log(randomBias);
+        }
+
+        if (e.code === "ArrowUp") {
+            randomBias += 0.01;
+            if (randomBias >= 1) randomBias = 1;
+            console.log(randomBias);
+        }
     })
 
-    document.addEventListener('keyup', () => mouse.shift = false);
+    document.addEventListener('keyup', (e) => {
+        if (e.code === "ControlLeft" || e.code === "ControlRight") {
+            mouse.ctrl = false;
+        }
+        if (e.code === "ShiftLeft" || e.code === "ShiftRight") {
+            mouse.shift = false;
+        }
+        if (e.code === "Minus") mouse.minus = false;
+        if (e.code == "Equal") mouse.plus = false;
+
+        if (e.code === 'KeyP') {
+            simulationOn = !simulationOn;
+            document.title = simulationOn ? "Simulating..." : "Game Of Life";
+        }
+
+        if (e.code === "KeyF") {
+            // full screen
+            UNIT_WIDTH = Math.floor(Math.max(WIDTH, HEIGHT) / Math.max(gridMap[0].length, gridMap.length));
+            camera.x = 0;
+            camera.y = 0;
+        }
+
+        if (e.code === "Backspace") {
+            for (let y = 0; y < gridMap.length; y++) {
+                for (let x = 0; x < gridMap[0].length; x++) {
+                    gridMap[y][x] = false;
+                }
+            }
+        }
+
+        if (e.code === "KeyR") {
+            // create random map
+            for (let y = 0; y < gridMap.length; y++) {
+                for (let x = 0; x < gridMap[y].length; x++) {
+                    gridMap[y][x] = Math.random() - randomBias >= 0;
+                }
+            }
+        }
+
+        if (e.code === "KeyA") {
+            addWidth = addWidth - 1 >= 0 ? addWidth - 1 : addWidth;
+        }
+
+        if (e.code === "KeyS") {
+            addWidth++;
+        }
+    }
+    );
 
     document.addEventListener("mousemove", (e) => {
         mouse.x = e.x;
@@ -73,18 +193,11 @@ function listeners() {
         mouse.left = false;
         mouse.right = false;
     })
-
-    document.addEventListener('keyup', (e) => {
-        if (e.code === 'KeyP') {
-            simulationOn = !simulationOn;
-            document.title = simulationOn ? "Simulating..." : "Game Of Life";
-        }
-    })
 }
 
 function reset() {
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = document.body.style.backgroundColor;
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
 }
 
@@ -128,17 +241,32 @@ function simulate() {
 function render() {
     reset();
 
-    for (let y=0; y < gridMap.length; y++) {
-        for (let x=0; x < gridMap[y].length; x++) {
-            ctx.fillStyle = gridMap[y][x] ? "white" : "black";
-            if (Math.floor(mouse.x / UNIT_WIDTH) + Math.floor(camera.x / UNIT_WIDTH) === x && Math.floor(mouse.y / UNIT_WIDTH) - Math.floor(camera.y / UNIT_WIDTH) === y) {
-                if(!gridMap[y][x]) ctx.fillStyle = "gray";
+    if (keys.minus || keys.plus) {
+        if (mouse.shift) {
+            SIMULATION_FPS = keys.minus ? SIMULATION_FPS - 1 >= 0 ? SIMULATION_FPS - 1 : SIMULATION_FPS : SIMULATION_FPS + 1;
+        }
+        else {
+            UNIT_WIDTH = keys.minus ? UNIT_WIDTH - time.deltaTime >= 0 ? UNIT_WIDTH - time.deltaTime : UNIT_WIDTH : UNIT_WIDTH + time.deltaTime;
+
+            camera.x -= time.deltaTime * UNIT_WIDTH / 2;
+            camera.y -= time.deltaTime * UNIT_WIDTH / 2;
+
+            if (keys.minus) {
+                if (UNIT_WIDTH - time.deltaTime >= 0) {
+                    UNIT_WIDTH -= time.deltaTime;
+                    camera.x -= time.deltaTime * UNIT_WIDTH;
+                    camera.y += time.deltaTime * UNIT_WIDTH;
+                }
             }
-            ctx.fillRect(x * UNIT_WIDTH - camera.x + mouse.cameraOffset.x, y * UNIT_WIDTH + camera.y - mouse.cameraOffset.y, UNIT_WIDTH, UNIT_WIDTH);
+            else {
+                UNIT_WIDTH += time.deltaTime;
+                camera.x += time.deltaTime * UNIT_WIDTH;
+                camera.y -= time.deltaTime * UNIT_WIDTH;
+            }
         }
     }
 
-    if (mouse.shift && mouse.left) { // camera movement
+    if (mouse.ctrl && mouse.left) { // camera movement
         if (mouse.cameraOffset.lastHeldPosition.x === null) {
             mouse.cameraOffset.lastHeldPosition = {
                 x: mouse.x,
@@ -167,6 +295,13 @@ function render() {
             let y = Math.floor(mouse.y / UNIT_WIDTH) - Math.floor(camera.y / UNIT_WIDTH);
 
             gridMap[y][x] = mouse.left;
+            for (let _y = 0; _y < addWidth; _y++) {
+                for (let _x = 0; _x < addWidth; _x++) {
+                    if (x + _x >= 0 && x + _x < gridMap[0].length && y + _y >= 0 && y + _y < gridMap.length) {
+                        gridMap[y + _y][x + _x] = mouse.left;
+                    }
+                }
+            }
         }
     }
 
@@ -179,6 +314,21 @@ function render() {
             simulationFrameCount++;
         }
     }
+
+    for (let y=0; y < gridMap.length; y++) {
+        for (let x=0; x < gridMap[y].length; x++) {
+            ctx.fillStyle = gridMap[y][x] ? LIVE_CELL_COLOR : DEAD_CELL_COLOR;
+            if (Math.floor(mouse.x / UNIT_WIDTH) + Math.floor(camera.x / UNIT_WIDTH) === x && Math.floor(mouse.y / UNIT_WIDTH) - Math.floor(camera.y / UNIT_WIDTH) === y) {
+                if(!gridMap[y][x]) {
+                    ctx.fillStyle = "gray";
+                }
+            }
+            
+            ctx.fillRect(x * UNIT_WIDTH - camera.x + mouse.cameraOffset.x, y * UNIT_WIDTH +  camera.y - mouse.cameraOffset.y, UNIT_WIDTH, UNIT_WIDTH);
+        }
+    }
+
+    time.updateTime();
 
     setTimeout(render, 1000 / RENDER_FPS)
 }
